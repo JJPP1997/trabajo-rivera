dd966ebd765b2b7eab25946ed5d15d45d840db3e
tenedor <queteimportaloquedigo16@gmail.com>
2021-01-08 21:06:50 +0100

    started blog 08/1

--- scripts/scriptsBoletin.js
+++ scripts/scriptsBoletin.js
@@ -5,12 +5,7 @@ function loader(){
 	var height = $(window).height();
 	var menubar=$("#wholeHeader").height();
 	//fit overlay
-	$("#title_overlay").height($("#title_background").height);
-	$("#title_overlay").css('top', menubar+5);
-	$("#title_background").width(width).height(height);
 	$("#overlay").width(width).height(height);
-	$("#overlay_center").width(width-100).height($("#title_background").height);
-	$(".overlay_margin").width(50).height($("#title_background").height);
 	//hide slides
 	var slides = document.getElementsByClassName("mySlides");
 	for (i = 0; i < slides.length; i++) {
@@ -53,36 +48,9 @@ function loader(){
 	//fadeBody(true);
 	
 }
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function AutoPlusSlides() {
-  showSlides(slideIndex += 1);
-   triggerTitleAnimation();
-	
-}
-function plusSlides(n) {
-	showSlides(slideIndex += n);
-	 triggerTitleAnimation();
-	clicked=true;
-}
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
- 
-}
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-
-}
 function fadeBody(fade) {
 	//hides t shows the ovverlay to hide the loading of elements
 	if(fade){
@@ -96,26 +64,8 @@ function fadeBody(fade) {
         });
 	}
 }
-function triggerTitleAnimation(){
-	var tlt=$( "#title" ).clone();
-	$( "#title" ).remove();
-	tlt.appendTo( "#overlay_center" );
-
-}
-function readMore(container,open){
-	console.log(container);
-	if(open){
-		$( "#"+ container ).find("p").css({"overflow": "auto", "display": "inline"});
-		$( "#"+ container ).find("button").attr("onclick","readMore('"+container+"',false)");
-		$( "#"+ container ).find("button").text("Leer menos");
-		
-	}else{
-		$( "#"+ container ).find("p").css({"overflow": "hidden","display": "block" });
-		$( "#"+ container ).find("button").attr("onclick","readMore('"+container+"',true)");
-		$( "#"+ container ).find("button").text("Leer más");
-		
-	}
-	
+function openNav(x) {
+  x.classList.toggle("change");
 }
 function resizer(){
 	
